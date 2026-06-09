269744d87fc6a6daaafba3f2b962cd26a7b61913
lyphucthien <lythien180312@gmail.com>
2026-06-09 17:17:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ function formatUptime() {
     const days = Math.floor(totalSeconds / 86400);
     const hours = Math.floor((totalSeconds % 86400) / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const secs = Math.floor(seconds % 60);
 
-    return `${days} ngày ${hours} giờ ${minutes} phút`;
+    return `${days} ngày ${hours} giờ ${minutes} phút ${secs} giây`;
 }
 
 app.get("/", (req, res) => {
