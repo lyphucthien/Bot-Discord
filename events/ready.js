16e5e2ed08adcee4da2eaf10b1d93c35627f5ff6
lyphucthien <lythien180312@gmail.com>
2026-06-10 17:57:36 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,12 @@ module.exports = (client) => {
 
         console.log(`✅ ${client.user.tag} Đã Online`);
 
+        const commands = await client.application.commands.fetch();
+
+        console.log(
+            "Slash Commands:",
+            commands.map(cmd => cmd.name)
+        );
+
     });
 };
