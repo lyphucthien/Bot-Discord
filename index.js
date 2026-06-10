7d351f58e6a2e513f35318e02fa023ae11131f77
lyphucthien <lythien180312@gmail.com>
2026-06-10 17:52:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences,
         GatewayIntentBits.MessageContent
     ]
 });
