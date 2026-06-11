2c88819bc652571fd21054d1067f58b134f21ca7
lyphucthien <lythien180312@gmail.com>
2026-06-11 11:07:20 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,9 @@ module.exports = (client) => {
         console.log(`✅ ${client.user.tag} Đã Online`);
 
         const commands = await client.application.commands.fetch();
+        const channel = guild.channels.cache.get('1514454494119858206');
 
+        await channel.setName('TEST OWNER');
         console.log(
             "Slash Commands:",
             commands.map(cmd => cmd.name)
