7a35dfbab3817ef0ddb85b75f99f3a240c57763f
lyphucthien <lythien180312@gmail.com>
2026-06-11 14:34:22 +0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -171,11 +171,11 @@ module.exports = (client) => {
                     };
 
                     await channel.send({
-                        content: `<@&${config.ticketPingRole}> 🚨 Có ticket mới được tạo!`,
+                        content: `<@&${config.staffRole}> 🚨 Có ticket mới được tạo!`,
                         embeds: [embed],
                         components: [row],
                         allowed_mentions: {
-                            roles: [config.ticketPingRole] // ⚡ FIX ĐÚNG ROLE
+                            roles: [config.staffRole] // ⚡ FIX ĐÚNG ROLE
                         }
                     });
 
