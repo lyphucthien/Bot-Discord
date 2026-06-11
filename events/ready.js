0fde4a6a1cf074a06ea992d747e7bc9a91747c8d
lyphucthien <lythien180312@gmail.com>
2026-06-11 11:09:39 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,9 +2,11 @@ module.exports = (client) => {
     client.once('clientReady', async () => {
 
         console.log(`✅ ${client.user.tag} Đã Online`);
+        const guild = client.guilds.cache.first();
 
-        const commands = await client.application.commands.fetch();
-        const channel = guild.channels.cache.get('1514454494119858206');
+        const channel = guild.channels.cache.get(
+            '1514454494119858206'
+        );
 
         await channel.setName('TEST OWNER');
         console.log(
