a0ce59763127d7e63082f77a4de601715a17c9e4
lyphucthien <lythien180312@gmail.com>
2026-06-12 16:58:23 +0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -105,7 +105,7 @@ module.exports = (client) => {
                                     PermissionsBitField.Flags.ReadMessageHistory,
                                 ],
                             },
-                            ...(config.staffRoles || []).map(roleId => ({
+                            ...(config.Helper || []).map(roleId => ({
                                 id: roleId,
                                 allow: [
                                     PermissionsBitField.Flags.ViewChannel,
