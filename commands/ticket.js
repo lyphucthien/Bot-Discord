32761dcaf2df5e3ec86616eca82ef245c85009c8
lyphucthien <lythien180312@gmail.com>
2026-06-12 16:25:42 +0700

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -11,9 +11,18 @@ module.exports = {
             .setTitle('🎫 Ticket System')
             .setDescription(
                 `📌 Chọn loại ticket bạn muốn tạo:\n\n` +
-                `🛠️ Support → Hỗ Trợ\n` +
-                `🚨 Report → Báo Cáo Người Dùng / Lỗi\n` +
-                `🛒 Order → Đặt Hàng / Mua Dịch Vụ\n\n` +
+                `🛠️ Support:\n` +
+                `→ Hỗ Trợ Kỹ Thuật\n` +
+                `→ Lỗi Bot / Hệ Thống\n` +
+
+                `🚨 Report:\n` +
+                `→ Báo Cáo Người Chơi Phạm Luật\n` +
+                `→ Spam / Scam / Lừa Đảo (Kể Cả Link)\n` +
+
+                `🛒 Order:\n` +
+                `→ Hỗ Trợ Đặt / Thanh Toán / Kiểm Tra Đơn Hàng\n` +
+                `→ Lỗi Giao Dịch / Thắc Mắc Về Sản Phẩm\n` +
+
                 `⏱ Thời Gian Staff Phản Hồi: 1-60 Phút\n` +
                 `📦 Version: v1.0 (Test)`,
             )
