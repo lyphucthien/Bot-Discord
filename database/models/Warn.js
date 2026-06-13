7da16d19cea2a01092f88cb38b1d39f6629010b5
lyphucthien <lythien180312@gmail.com>
2026-06-13 17:58:00 +0700

    Update Warn.js

--- database/models/Warn.js
+++ database/models/Warn.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
 
 const warnSchema = new mongoose.Schema({
-
-    guildId: String,
     userId: String,
-
+    guildId: String,
     warns: {
         type: Number,
         default: 0
-    }
-
+    },
+    reasons: [String]
 });
 
 module.exports = mongoose.model('Warn', warnSchema);
