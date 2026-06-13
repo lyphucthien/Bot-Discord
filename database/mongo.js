5b4d74cf1754f47b631e7e5c0aded4a9006819c2
lyphucthien <lythien180312@gmail.com>
2026-06-13 18:31:57 +0700

    Update mongo.js

--- database/mongo.js
+++ database/mongo.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+
+        await mongoose.connect(process.env.MONGO_URI);
 
         console.log('🟢 MongoDB Đã Kết Nối Thành Công!');
     } catch (err) {
