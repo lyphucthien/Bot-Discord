f24c398acbe9903a7df4d81ce3dbec83aafc4265
lyphucthien <lythien180312@gmail.com>
2026-06-14 13:32:12 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,15 +5,31 @@ module.exports = (client) => {
         console.log(`✅ ${client.user.tag} Đã Online`);
 
         const commands = await client.application.commands.fetch();
-
         console.log(
             "📌 Slash Commands:",
             commands.map(cmd => cmd.name).join(', ')
         );
 
-        client.user.setActivity('Discord Bot Pro', {
-            type: 2
-        });
+        const statuses = [
+            { name: "Discord Bot Pro", type: 3 }, // Watching
+            { name: "/help để xem lệnh", type: 0 }, // Playing
+            { name: `${client.guilds.cache.size} servers`, type: 3 },
+            { name: "users đang online", type: 2 } // Listening
+        ];
+        let i = 0;
+        setInterval(() => {
+            const status = statuses[i];
+
+            client.user.setActivity(status.name, {
+                type: status.type
+            });
+
+            i++;
+
+            if (i >= statuses.length) i = 0;
+
+        }, 5000);
+
     });
 
 };
