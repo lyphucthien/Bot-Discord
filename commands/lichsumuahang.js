d373605189c8b8efcb4eaa10090a0125296d8ab1
lyphucthien <lythien180312@gmail.com>
2026-06-16 14:31:30 +0700

    Update lichsumuahang.js

--- commands/lichsumuahang.js
+++ commands/lichsumuahang.js
@@ -9,7 +9,8 @@ module.exports = {
 
     async execute(interaction) {
 
-        const orders = orderDB.getUser(interaction.user.id);
+        const userId = interaction.user.id;
+        const orders = orderDB.getUser(userId);
 
         if (!orders.length) {
             return interaction.reply({
@@ -29,29 +30,27 @@ module.exports = {
                 sum + Number(order.price || 0),
             0
         );
-        
-        const member = interaction.guild.members.cache.get(userId);
-        
+
         let rank = '👤 Thành Viên';
 
-        if (totalSpent >= 5000)
-            rank = `<@&1516035637118107678>`;
-        else if (totalSpent >= 50000)
-            rank = `<@&1516035775240867961>`;
-        else if (totalSpent >= 100000)
-            rank = `<@&1516045762360774696>`;
-        else if (totalSpent >= 200000)
-            rank = `<@&1516045778089410700>`;
-        else if (totalSpent >= 500000)
-            rank = `<@&1516045780228640778>`;
-        else if (totalSpent >= 1000000)
-            rank = `<@&1516045781164101722>`;
-        else if (totalSpent >= 2000000)
-            rank = `<@&1516045781881196595>`;
+        if (totalSpent >= 10000000)
+            rank = `<@&1516045783865233579>`;
         else if (totalSpent >= 5000000)
             rank = `<@&1516045782858465381>`;
-        else if (totalSpent >= 10000000)
-            rank = `<@&1516045783865233579>`;
+        else if (totalSpent >= 2000000)
+            rank = `<@&1516045781881196595>`;
+        else if (totalSpent >= 1000000)
+            rank = `<@&1516045781164101722>`;
+        else if (totalSpent >= 500000)
+            rank = `<@&1516045780228640778>`;
+        else if (totalSpent >= 200000)
+            rank = `<@&1516045778089410700>`;
+        else if (totalSpent >= 100000)
+            rank = `<@&1516045762360774696>`;
+        else if (totalSpent >= 50000)
+            rank = `<@&1516035775240867961>`;
+        else if (totalSpent >= 5000)
+            rank = `<@&1516035637118107678>`;
 
         function generateEmbed() {
 
