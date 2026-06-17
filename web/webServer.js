7a2e79a6256860565805fc8029b44c9de6282d70
lyphucthien <lythien180312@gmail.com>
2026-06-17 14:26:23 +0700

    Update webServer.js

--- web/webServer.js
+++ web/webServer.js
@@ -80,7 +80,7 @@ module.exports = (client) => {
     --bg3:#0f172a;
     --bg4:#2563eb;
 
-    --card:rgba(255,255,255,.1);
+    --card:rgba(255,255,255,0.5);
     --text:white;
     --border:rgba(255,255,255,.15);
     --stat:rgba(255,255,255,.05);
