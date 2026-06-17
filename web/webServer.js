a4760a2451b5ba272dffc2a83a0cec2aa99eaaa2
lyphucthien <lythien180312@gmail.com>
2026-06-17 13:42:41 +0700

    Update webServer.js

--- web/webServer.js
+++ web/webServer.js
@@ -297,7 +297,7 @@ html.light .loading-spinner{
     display:none;
     position:fixed;
     inset:0;
-    background:rgba(0,0,0,0.2);
+    background:rgba(0,0,0,0.4);
     backdrop-filter:blur(8px);
     justify-content:center;
     align-items:center;
