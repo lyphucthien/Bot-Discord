f8e9e0a6c35640cdfa228eb62b110d021fe5d367
lyphucthien <lythien180312@gmail.com>
2026-06-27 11:54:03 +0700

    Delete web/login-admin directory

--- web/login-admin/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    admin: {
-        username: "LyPhucThien",
-        password: "lypthien180312."
-    },
-    sessionSecret: "my_super_secret_key_123"
-};
--- web/login-admin/middleware.js
+++
@@ -1,8 +0,0 @@
-function isAdmin(req, res, next) {
-    if (req.session && req.session.admin) {
-        return next();
-    }
-    return res.redirect("/login");
-}
-
-module.exports = { isAdmin };
