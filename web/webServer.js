3042f9573aef9ce0d3bc07ba912d54bdefafd9e0
lyphucthien <lythien180312@gmail.com>
2026-06-30 14:42:03 +0700

    Update webServer.js

--- web/webServer.js
+++ web/webServer.js
@@ -1467,7 +1467,38 @@ body {
                     title: "Dù Có Cách Xa",
                     artist: "Đinh Mạnh Ninh",
                     src: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Dù%20Có%20Cách%20Xa.mp3",
-                    cover: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/"
+                    cover: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Dù%20Có%20Cách%20Xa.jpg"
+                },
+
+                {
+                    title: "Để Giành Khi Thức Giấc",
+                    artist: "SIVAN",
+                    src: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Để%20Dành%20Khi%20Thức%20Giấc.mp3",
+                    cover: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Để%20Dành%20Khi%20Thức%20Giấc.jpg"
+                },
+                {
+                    title: "Vệt Nắng Nhạt Phai (Piano Version)",
+                    artist: "Kai, Xưa Lâm, LIKIE",
+                    src: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Vệt%20Nắng%20Nhạt%20Phai%20(Piano%20Version).mp3",
+                    cover: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Vệt%20Nắng%20Nhạt%20Phai%20(Piano%20Version).jpg"
+                },
+                {
+                    title: "Già Cùng Nhau Là Được",
+                    artist: "Tùng TeA, PC, TaynguyenSound",
+                    src: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Già%20Cùng%20Nhau%20Là%20Được.mp3",
+                    cover: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Già%20Cùng%20Nhau%20Là%20Được.jpg"
+                },
+                {
+                    title: "Feel At Home",
+                    artist: "B Ray",
+                    src: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Feel%20At%20Home.mp3",
+                    cover: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Feel%20At%20Home.jpg"
+                },
+                {
+                    title: "Bản Nhạc Cuối (Cho Em)",
+                    artist: "B Ray",
+                    src: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Bản%20Nhạc%20Cuối%20(Cho%20Em).mp3",
+                    cover: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Bản%20Nhạc%20Cuối%20(Cho%20Em).jpg"
                 },
             ];
 
