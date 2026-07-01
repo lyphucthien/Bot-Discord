81b38d9f8468f63025e7b0675990bf8b8606e750
lyphucthien <lythien180312@gmail.com>
2026-07-01 11:41:11 +0700

    Update webServer.js

--- web/webServer.js
+++ web/webServer.js
@@ -1143,26 +1143,6 @@ body {
     flex:1;
 }
 
-#musicOverlay{
-    position: fixed;
-    inset: 0;
-
-    background: rgba(0,0,0,.45);
-    backdrop-filter: blur(4px);
-
-    opacity: 0;
-    visibility: hidden;
-
-    transition: .3s;
-
-    z-index: 9998;
-}
-
-#musicOverlay.show{
-    opacity: 1;
-    visibility: visible;
-}
-
 .no-animation *,
 .no-animation *::before,
 .no-animation *::after{
@@ -1860,19 +1840,6 @@ body {
                 const sideMenu = document.getElementById("sideMenu");
                 const menuOverlay = document.getElementById("menuOverlay");
                 const closeMenu = document.getElementById("closeMenu");
-                const musicOverlay = document.getElementById("musicOverlay");
-                const musicBar = document.getElementById("musicBar");
-
-                musicOverlay.addEventListener("click", () => {
-                    musicBar.style.display = "none";
-                    musicOverlay.classList.remove("show");
-
-                    settings.music = false;
-                    saveSettings();
-
-                    const musicToggle = document.getElementById("musicToggle");
-                    if (musicToggle) musicToggle.checked = false;
-                });
 
                 menuBtn.addEventListener("click", () => {
                     document.body.classList.add("menu-open");
@@ -2073,22 +2040,6 @@ body {
                     ramChartCard.style.display = settings.showRamChart ? "" : "none";
                 }
 
-                const musicBar = document.getElementById("musicBar");
-                const musicOverlay = document.getElementById("musicOverlay");
-
-                if (musicBar) {
-
-                    if (settings.music) {
-                        musicBar.style.display = "flex";
-                        musicOverlay.classList.add("show");
-
-                    } else {
-                        musicBar.style.display = "none";
-                        musicOverlay.classList.remove("show");
-
-                    }
-                }
-
             }
 
             document.addEventListener("DOMContentLoaded", () => {
@@ -2212,8 +2163,6 @@ body {
                 </div>
 
             </div>
-
-            <div id="musicOverlay"></div>
 
             <div id="musicBar" class="music-player">
 
