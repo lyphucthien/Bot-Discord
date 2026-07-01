718ae7cf5652620d9607f654f0a0c66cefa2fc37
lyphucthien <lythien180312@gmail.com>
2026-07-01 14:43:23 +0700

    Update webServer.js

--- web/webServer.js
+++ web/webServer.js
@@ -1451,10 +1451,10 @@ body.menu-open .music-player {
 
             const playlist = [
                 {
-                    title: "Buông Bỏ Sự Phụ Thuộc Nơi Anh (离开我的依赖)",
+                    title: "Buông Bỏ Sự Phụ Thuộc Nơi Anh",
                     artist: "Vương Diễm Vi",
-                    src: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Buông%20Bỏ%20Sự%20Phụ%20Thuộc%20Nơi%20Anh%20(离开我的依赖).mp3",
-                    cover: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Buông%20Bỏ%20Sự%20Phụ%20Thuộc%20Nơi%20Anh%20(离开我的依赖).jpg"
+                    src: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Buông%20Bỏ%20Sự%20Phụ%20Thuộc%20Nơi%20Anh.mp3",
+                    cover: "https://raw.githubusercontent.com/lyphucthien/pictuces-music/main/Buông%20Bỏ%20Sự%20Phụ%20Thuộc%20Nơi%20Anh.jpg"
                 },
                 {
                     title: "WILDFIRE",
