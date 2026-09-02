b4804b735352adad7bf75c3d00305856d7056fbb
lyphucthien <lythien180312@gmail.com>
2026-09-02 13:47:15 +0700

    Update getkey.js

--- commands/getkey.js
+++ commands/getkey.js
@@ -3,12 +3,12 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('getkey')
-        .setDescription('Lấy key/link truy cập'),
+        .setDescription('Lấy Key Xác Thực Server MC'),
     async execute(interaction) {
-        const link = 'https://link4sub.com/owycAYxKdu'; // đổi thành link thật
+        const link = 'https://link4sub.com/owycAYxKdu';
 
         await interaction.reply({
-            content: `🔑 Đây là link của bạn:\n${link}`,
+            content: `🔑 Đây là link của bạn:\n**GetKey >** [Get Key](${link})`,
             ephemeral: true
         });
     },
