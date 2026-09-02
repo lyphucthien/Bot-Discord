d083e5e2fa2c870b32d0e21ac2185bbe37f99822
lyphucthien <lythien180312@gmail.com>
2026-09-02 14:07:49 +0700

    Add files via upload

---
+++ commands/key.js
@@ -0,0 +1,34 @@
+const {SlashCommandBuilder,ModalBuilder,TextInputBuilder,TextInputStyle,ActionRowBuilder} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('key')
+        .setDescription('Xác thực Key Minecraft'),
+
+    async execute(interaction) {
+
+        const modal = new ModalBuilder()
+            .setCustomId('keyVerifyModal')
+            .setTitle('Xác Thực Key Minecraft');
+
+        const mcInput = new TextInputBuilder()
+            .setCustomId('mcUsername')
+            .setLabel('Tên tài khoản Minecraft')
+            .setStyle(TextInputStyle.Short)
+            .setMaxLength(16)
+            .setRequired(true);
+
+        const keyInput = new TextInputBuilder()
+            .setCustomId('mcKey')
+            .setLabel('Key')
+            .setStyle(TextInputStyle.Short)
+            .setRequired(true);
+
+        modal.addComponents(
+            new ActionRowBuilder().addComponents(mcInput),
+            new ActionRowBuilder().addComponents(keyInput)
+        );
+
+        await interaction.showModal(modal);
+    },
+};
